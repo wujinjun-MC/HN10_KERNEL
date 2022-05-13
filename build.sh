ceb06148f5f07034a7da4ee55086d9c578f97c57
wujinjun-MC <89054683+wujinjun-MC@users.noreply.github.com>
2022-05-13 14:20:19 +0800

    设置定制版path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/acuicultor/Android/toolchains/aarch64-linux-android-4.9/bin
+export PATH=$PATH:/work/HN10_KERNEL/HN10_KERNEL/android-ndk-master/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin
 export CROSS_COMPILE=aarch64-linux-android-
 mkdir out
 make ARCH=arm64 O=out radioactive_defconfig
